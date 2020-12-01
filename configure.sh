af17a8bfa2b0bc127ec4f108ffa63790c6399a49
raindrops <47104993+raindrops2005@users.noreply.github.com>
2020-12-01 16:56:53 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,43 +1,37 @@
 #!/bin/sh
-
 # Download and install V2Ray
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-
-# Remove temporary directory
-rm -rf /tmp/v2ray
-
+curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+mkdir /usr/bin/v2ray /etc/v2ray
+touch /etc/v2ray/config.json
+unzip /v2ray.zip -d /usr/bin/v2ray
+# Remove /v2ray.zip and other useless files
+rm -rf /v2ray.zip /usr/bin/v2ray/*.sig /usr/bin/v2ray/doc /usr/bin/v2ray/*.json /usr/bin/v2ray/*.dat /usr/bin/v2ray/sys*
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+cat <<-EOF > /etc/v2ray/config.json
 {
-    "inbounds": [
+  "inbounds": [
+  {
+    "port": ${PORT},
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
         {
-            "port": $PORT,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID"
-                    }
-                ],
-				"decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws"
-            }
+          "id": "${UUID}",
+          "alterId": 64
         }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+      ]
+    },
+    "streamSettings": {
+      "network": "ws"
+    }
+  }
+  ],
+  "outbounds": [
+  {
+    "protocol": "freedom",
+    "settings": {}
+  }
+  ]
 }
 EOF
-
-# Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
