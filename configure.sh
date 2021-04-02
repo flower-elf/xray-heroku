6e713f5e9fad96852d5c1de98e149fb41be34e95
raindrops <47104993+raindrops2005@users.noreply.github.com>
2021-04-02 08:00:52 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@ touch /etc/v2ray/config.json
 unzip /v2ray.zip -d /usr/bin/v2ray
 # Remove /v2ray.zip and other useless files
 rm -rf /v2ray.zip /usr/bin/v2ray/*.sig /usr/bin/v2ray/doc /usr/bin/v2ray/*.json /usr/bin/v2ray/*.dat /usr/bin/v2ray/sys*
-# V2Ray new configuration
+# Xray new configuration
 cat <<-EOF > /etc/v2ray/config.json
 {
   "inbounds": [
