f2d850c5725a7cbe86caf0e815be6f7c14fcc4e7
rain_flower <47104993+flower-elf@users.noreply.github.com>
2021-07-08 11:43:06 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Download and install xray
-wget -q -O xray.zip http://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+wget -q -O https://raincloud.glaorg.top/data/public-share/xray/xray
 mkdir /usr/bin/Xray /etc/Xray
+mv xray /usr/bin/Xray/
 touch /etc/Xray/config.json
-unzip /xray.zip -d /usr/bin/Xray
+#unzip /xray.zip -d /usr/bin/Xray
 # Remove /xray.zip and other useless files
-rm -rf /xray.zip /usr/bin/Xray/LICENSE /usr/bin/Xray/*.md /usr/bin/Xray/*.dat
+#rm -rf /xray.zip /usr/bin/Xray/LICENSE /usr/bin/Xray/*.md /usr/bin/Xray/*.dat
 # xray new configuration
 cat <<-EOF > /etc/Xray/config.json
 {
