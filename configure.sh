a82961af2a37aec0359e05a80a2f2e218af2bb5d
rain_flower <47104993+flower-elf@users.noreply.github.com>
2021-10-14 09:33:02 +0800

    change domain to a var

--- configure.sh
+++ configure.sh
@@ -12,16 +12,7 @@ cat <<-EOF > /etc/Xray/config.json
         "rules": [
             {
                 "type": "field",
-                "domain": [
-                    "domain:t66y.com",
-		    "domain:epochtimes.com",
-		    "domain:minghui.org",
-		    "domain:pincong.rocks",
-		    "domain:dw.com",
-		    "domain:rfa.org",
-		    "domain:doubleclick.net",
-		    "domain:360.cn"
-                ],
+                "domain": [$rule],
                 "outboundTag": "block"
             },
 	    {
